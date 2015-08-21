43306659ec9588ba1128262baf7af95235a8873e
lwehui <lwehui@foxmail.com>
2015-08-21 14:23:42 +0800

    Update ch2_ex2_1.cpp

--- show image/ch2_ex2_1.cpp
+++ show image/ch2_ex2_1.cpp
@@ -1,13 +1,4 @@
-/* License:
-   Oct. 3, 2008
-   Right to use this code in any way you want without warrenty, support or any guarentee of it working.
 
-   BOOK: It would be nice if you cited it:
-   Learning OpenCV: Computer Vision with the OpenCV Library
-     by Gary Bradski and Adrian Kaehler
-     Published by O'Reilly Media, October 3, 2008*/
- 
-  
 #include "highgui.h"
 
 int main( int argc, char** argv )
